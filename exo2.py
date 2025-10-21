ef06a73b284762263466c138039d7a8d8732f7df
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 13:59:17 +0200

    refac 2A

--- exo2.py
+++ exo2.py
@@ -1,17 +1,11 @@
-# exo2.py
 import string
 
-# Table complète selon l'énoncé
 caracteres = string.ascii_letters + string.punctuation + string.digits + " "
 
 def crypt(message):
-    resultat = ""
-    for char in message:
-        if char in caracteres:
-            index = caracteres.index(char)
-            # on prend le caractère suivant, et on boucle si on atteint la fin
-            resultat += caracteres[(index + 1) % len(caracteres)]
-        else:
-            # caractères non listés sont inchangés
-            resultat += char
-    return resultat
+    def suivant(c):
+        if c in caracteres:
+            i = caracteres.index(c)
+            return caracteres[(i + 1) % len(caracteres)]
+        return c  # caractère inconnu inchangé
+    return "".join(suivant(c) for c in message)
