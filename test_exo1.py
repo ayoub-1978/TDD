85ee09604deb0612bb4208980ed8372bb23736d9
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 13:46:38 +0200

    fail 1C

--- test_exo1.py
+++ test_exo1.py
@@ -2,17 +2,17 @@ import unittest
 from exo1 import affiche
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_affiche_5(self):
+    def test_affiche_5_10(self):
+        attendu = "BuzzFizz78FizzBuzz"
+        self.assertEqual(affiche(5, 10), attendu)
+
+    def test_affiche_10_16(self):
+        attendu = "Buzz11Fizz1314FrisBee16"
+        self.assertEqual(affiche(10, 16), attendu)
+
+    def test_affiche_1_5(self):
         attendu = "12Fizz4Buzz"
-        self.assertEqual(affiche(5), attendu)
-
-    def test_affiche_15(self):
-        attendu = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee"
-        self.assertEqual(affiche(15), attendu)
-
-    def test_affiche_1(self):
-        attendu = "1"
-        self.assertEqual(affiche(1), attendu)
+        self.assertEqual(affiche(1, 5), attendu)
 
 if __name__ == "__main__":
     unittest.main()
