33f909418a859e8128b6e4f4b9979bdd67b867c6
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 14:06:58 +0200

    test fail 1C

--- test_exo2.py
+++ test_exo2.py
@@ -1,24 +1,37 @@
 import unittest
-from exo2 import crypt
+from exo2 import crypt, decrypt
 
 class TestCrypt(unittest.TestCase):
 
+    # Tests existants pour crypt
     def test_crypt_simple_pas_1(self):
-        # 'abc' + pas 1 -> 'bcd1'
         self.assertEqual(crypt("abc", 1), "bcd1")
 
     def test_crypt_simple_pas_3(self):
-        # 'abc' + pas 3 -> 'def3'
         self.assertEqual(crypt("abc", 3), "def3")
 
     def test_crypt_wrap(self):
-        # 'Z' + pas 1 -> '!' (suivant dans la table complète) + '1'
         self.assertEqual(crypt("Z", 1), "!1")
 
     def test_crypt_with_space(self):
-        # 'a a' + pas 2 selon la table complète -> 'cbc2'
         self.assertEqual(crypt("a a", 2), "cbc2")
 
+    # Nouveaux tests pour decrypt
+    def test_decrypt_simple(self):
+        message_crypte = crypt("abc", 1)
+        self.assertEqual(decrypt(message_crypte), "abc")
+
+    def test_decrypt_with_pas3(self):
+        message_crypte = crypt("hello", 3)
+        self.assertEqual(decrypt(message_crypte), "hello")
+
+    def test_decrypt_with_wrap(self):
+        message_crypte = crypt("Z", 1)
+        self.assertEqual(decrypt(message_crypte), "Z")
+
+    def test_decrypt_with_space(self):
+        message_crypte = crypt("a a", 2)
+        self.assertEqual(decrypt(message_crypte), "a a")
 
 if __name__ == "__main__":
     unittest.main()
