25d95577d22845951f885113eaa0fc1bc27211b6
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 13:47:28 +0200

    success 1C

--- exo1.py
+++ exo1.py
@@ -1,11 +1,12 @@
-def affiche(n):
-    def fizzbuzz_value(i):
+def affiche(n1, n2):
+    resultat = ""
+    for i in range(n1, n2 + 1):
         if i % 15 == 0:
-            return "FrisBee"
+            resultat += "FrisBee"
         elif i % 3 == 0:
-            return "Fizz"
+            resultat += "Fizz"
         elif i % 5 == 0:
-            return "Buzz"
-        return str(i)
-    
-    return "".join(fizzbuzz_value(i) for i in range(1, n + 1))
+            resultat += "Buzz"
+        else:
+            resultat += str(i)
+    return resultat
