79efa8cb868824809e774c804614564be49ee682
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 14:03:51 +0200

    success 2B

--- exo2.py
+++ exo2.py
@@ -2,10 +2,16 @@ import string
 
 caracteres = string.ascii_letters + string.punctuation + string.digits + " "
 
-def crypt(message):
-    def suivant(c):
+def crypt(message, pas):
+    """
+    Cryptage du message :
+    - Chaque caractère est décalé de 'pas' positions dans la table 'caracteres'
+    - Le pas est concaténé à la fin du message crypté
+    """
+    def decale(c):
         if c in caracteres:
             i = caracteres.index(c)
-            return caracteres[(i + 1) % len(caracteres)]
+            return caracteres[(i + pas) % len(caracteres)]
         return c  # caractère inconnu inchangé
-    return "".join(suivant(c) for c in message)
+
+    return "".join(decale(c) for c in message) + str(pas)
