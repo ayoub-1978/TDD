4354548b9328366122943b4e7857b90bb6aeef55
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 14:07:18 +0200

    success 2C

--- exo2.py
+++ exo2.py
@@ -13,3 +13,22 @@ def crypt(message, pas):
         caracteres[(caracteres.index(c) + pas) % len(caracteres)] if c in caracteres else c
         for c in message
     ) + str(pas)
+
+def decrypt(message):
+    """
+    Décrypte un message généré par crypt(message, pas)
+    - Le dernier caractère du message correspond au pas
+    """
+    # Récupérer le pas à partir du dernier caractère
+    pas = int(message[-1])
+    contenu = message[:-1]
+
+    resultat = ""
+    for c in contenu:
+        if c in caracteres:
+            i = caracteres.index(c)
+            # Décalage inverse
+            resultat += caracteres[(i - pas) % len(caracteres)]
+        else:
+            resultat += c
+    return resultat
