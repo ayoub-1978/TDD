eae7f48b300d8f050ae1aa3a87724e032f67e2c3
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 14:07:54 +0200

    refac 2C

--- exo2.py
+++ exo2.py
@@ -15,20 +15,9 @@ def crypt(message, pas):
     ) + str(pas)
 
 def decrypt(message):
-    """
-    Décrypte un message généré par crypt(message, pas)
-    - Le dernier caractère du message correspond au pas
-    """
-    # Récupérer le pas à partir du dernier caractère
     pas = int(message[-1])
     contenu = message[:-1]
-
-    resultat = ""
-    for c in contenu:
-        if c in caracteres:
-            i = caracteres.index(c)
-            # Décalage inverse
-            resultat += caracteres[(i - pas) % len(caracteres)]
-        else:
-            resultat += c
-    return resultat
+    return "".join(
+        caracteres[(caracteres.index(c) - pas) % len(caracteres)] if c in caracteres else c
+        for c in contenu
+    )
