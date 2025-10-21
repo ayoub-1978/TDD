5dbae4ccff39cde91beaf0ad0836f592dcb27027
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 13:36:52 +0200

    test success 1A

---
+++ exo1.py
@@ -0,0 +1,10 @@
+def affiche():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FrisBee", end="")
+        elif i % 3 == 0:
+            print("Fizz", end="")
+        elif i % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print(i, end="")
