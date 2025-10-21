5e86a120a7d570b9dab541770b5d0f268b49b3d0
ayoub-1978 <ayoub.benmoualli19@gmail.com>
2025-10-21 14:03:07 +0200

    test fail 2B

--- test_exo2.py
+++ test_exo2.py
@@ -2,19 +2,23 @@ import unittest
 from exo2 import crypt
 
 class TestCrypt(unittest.TestCase):
-    def test_crypt_simple(self):
-        # "abc" devient "bcd"
-        self.assertEqual(crypt("abc"), "bcd")
 
-    def test_crypt_with_wrap(self):
-        # selon la table complète (ascii_letters + punctuation + digits + " "),
-        # le caractère suivant de 'Z' est '!' (pas 'a')
-        self.assertEqual(crypt("Z"), "!")
+    def test_crypt_simple_pas_1(self):
+        # 'abc' + pas 1 -> 'bcd1'
+        self.assertEqual(crypt("abc", 1), "bcd1")
+
+    def test_crypt_simple_pas_3(self):
+        # 'abc' + pas 3 -> 'def3'
+        self.assertEqual(crypt("abc", 3), "def3")
+
+    def test_crypt_wrap(self):
+        # 'Z' + pas 1 -> '!' (suivant dans la table complète) + '1'
+        self.assertEqual(crypt("Z", 1), "!1")
 
     def test_crypt_with_space(self):
-        # l'espace est dans la table, donc il se déplace aussi
-        result = crypt("a a")
-        self.assertEqual(len(result), 3)
+        # 'a a' + pas 2 selon la table complète -> 'cbc2'
+        self.assertEqual(crypt("a a", 2), "cbc2")
+
 
 if __name__ == "__main__":
     unittest.main()
